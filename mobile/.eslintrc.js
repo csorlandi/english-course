7e5d2bc86c92c2056d56e4bdac665914e72805ed
Claudio Orlandi <cso.junior1996@gmail.com>
2020-05-21 03:10:20 -0300

    feat: setup linters
    Install and configure ESLint, Prettier and Editor Config on project and set default Log Box to new Beta Version.

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,4 +1,43 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react', 'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
--- mobile/index.js
+++ mobile/index.js
@@ -1,9 +1,7 @@
-/**
- * @format
- */
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+import { name as appName } from './app.json';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+require('react-native').unstable_enableLogBox();
 
 AppRegistry.registerComponent(appName, () => App);
