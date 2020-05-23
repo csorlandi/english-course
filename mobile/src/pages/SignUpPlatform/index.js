5cbfd63728076fe001dd237532c649244892a7a5
Claudio Orlandi <cso.junior1996@gmail.com>
2020-05-22 21:43:39 -0300

    feat: SignUp screens
    Start to code SignUp Stack Screens, need to check vector icons link and react-native-progress default sizes (we got stuck on layout).

--- mobile/src/pages/SignUpPlatform/index.js
+++ mobile/src/pages/SignUpPlatform/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
 
+import { colors } from '~/styles';
+
 import * as Styled from './styles';
 
 export default function SignUpPlatform() {
   return (
     <Styled.Container>
-      <Styled.Title>Página SignUpPlatform</Styled.Title>
+      <Styled.HeaderContainer>
+        <Styled.BackIcon />
+        <Styled.Progress />
+      </Styled.HeaderContainer>
+      <Styled.TitleContainer>
+        <Styled.Title>Escolha a forma de cadastro</Styled.Title>
+        <Styled.Description>
+          Isso vai te ajudar a ter acesso à Área de Alunos
+        </Styled.Description>
+      </Styled.TitleContainer>
+      <Styled.OptionsContainer>
+        <Styled.Option color={colors.danger} first>
+          <Styled.OptionIcon color={colors.danger} name="facebook-f" />
+          <Styled.OptionName color={colors.danger}>Google</Styled.OptionName>
+        </Styled.Option>
+        <Styled.Option color={colors.success}>
+          <Styled.OptionIcon color={colors.success} name="at" />
+          <Styled.OptionName color={colors.success}>Email</Styled.OptionName>
+        </Styled.Option>
+        <Styled.Option color={colors.primary}>
+          <Styled.OptionIcon color={colors.primary} name="google" />
+          <Styled.OptionName color={colors.primary}>Facebook</Styled.OptionName>
+        </Styled.Option>
+      </Styled.OptionsContainer>
     </Styled.Container>
   );
 }
